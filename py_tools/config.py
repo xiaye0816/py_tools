4fb8c82d6a8abfe4975683a86b050c29c5fee0ef
xiaye0816 <xiaye@shaoguoqing.com>
2019-05-19 13:37:21 +0800

    增加天行api key

--- py_tools/config.py
+++ py_tools/config.py
@@ -9,12 +9,16 @@ config.read(rootPath + '/.env')
 
 section_default = config['default']
 section_ding_talk = config['ding_talk']
+section_tian_api = config['tian_api']
 
 APPLICATION_NAME = section_default['application_name']
 
 DING_TALK_ROBOT_ACCESS_TOKEN = section_ding_talk['robot_access_token']
 
+TIAN_API_KEY = section_tian_api['api_key']
+
 __all__ = [
     'APPLICATION_NAME',
-    'DING_TALK_ROBOT_ACCESS_TOKEN'
+    'DING_TALK_ROBOT_ACCESS_TOKEN',
+    'TIAN_API_KEY'
 ]
--- py_tools/test/test.py
+++ py_tools/test/test.py
@@ -1,8 +1,9 @@
 from py_tools.config import APPLICATION_NAME
 from py_tools.config import DING_TALK_ROBOT_ACCESS_TOKEN
+from py_tools.config import TIAN_API_KEY
 from py_tools.notify.ding_talk import ding_talk_notify_util
-
 
 print(APPLICATION_NAME)
 print(DING_TALK_ROBOT_ACCESS_TOKEN)
+print(TIAN_API_KEY)
 ding_talk_notify_util.send_robot_notify('1234569')
