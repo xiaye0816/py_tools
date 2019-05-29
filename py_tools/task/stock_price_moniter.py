b2aecd1d91ddbda3517706900868135586eb6d7e
xiaye0816 <xiaye@shaoguoqing.com>
2019-05-29 13:36:39 +0800

    优化逻辑

--- py_tools/task/stock_price_moniter.py
+++ py_tools/task/stock_price_moniter.py
@@ -18,26 +18,31 @@ def func():
 
     while True:
 
-        # 查询实时数据
-        stock_data = stock_data_util.stock_data_query(stock_code)
+        try:
 
-        stock_name = stock_data[0]
-        base_price = float(stock_data[2])
-        current_price = float(stock_data[3])
+            # 查询实时数据
+            stock_data = stock_data_util.stock_data_query(stock_code)
 
-        print("%s 当前: %s" % (stock_name, current_price))
+            stock_name = stock_data[0]
+            base_price = float(stock_data[2])
+            current_price = float(stock_data[3])
 
-        if not notified_price or abs(current_price - notified_price) >= trigger_notify_price:
+            print("%s 当前: %s" % (stock_name, current_price))
 
-            notified_price = current_price
-            # trigger_notify_price = base_price / 10
+            if not notified_price or abs(current_price - notified_price) >= trigger_notify_price:
+                notified_price = current_price
+                # trigger_notify_price = base_price / 10
 
-            ding_talk_notify_util.send_robot_notify(
-                notify_text % (
-                    stock_name, current_price, base_price,
-                    round((current_price - base_price) / base_price * 100, 2)))
+                ding_talk_notify_util.send_robot_notify(
+                    notify_text % (
+                        stock_name, current_price, base_price,
+                        round((current_price - base_price) / base_price * 100, 2)))
 
-            fast_check_count = 5
+                fast_check_count = 5
+
+        except:
+
+            print("查询出错了...")
 
         if fast_check_count > 0:
             time.sleep(2)
