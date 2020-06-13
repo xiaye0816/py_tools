2d7a5b01d7ccf71cc7190fbe81b756d8f5cb6ee4
xiaye0816 <xiaye@shaoguoqing.com>
2020-06-13 22:44:39 +0800

    增加goodsId

--- py_tools/task/check_rishang_stock_task.py
+++ py_tools/task/check_rishang_stock_task.py
@@ -1,6 +1,8 @@
 from py_tools.data.rishang import rishang_util
 
-# 赫莲娜（HR）活颜修护舒缓日霜晚霜套装 2c9194587219d0ae017219dc8f4c02e0
+# 赫莲娜（HR）活颜修护舒缓日霜晚霜套装       2c9194587219d0ae017219dc8f4c02e0
+# 植村秀琥珀洁颜油2瓶装                   2c9194587219d0ae017219dc9aeb0b69
+# 赫莲娜（HR）绿宝瓶亮眼精灵精华乳双支套装   2c9194597219d0ad017219dc8f7a02fe
 
 while True:
     open_booking = rishang_util.check_rishang_stock('2c9194587219d0ae017219dc8f4c02e0')
